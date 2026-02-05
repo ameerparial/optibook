84cf5e151081574af6d47a5590a945617e7afa70
ameerparial <khuhroameerhamza4@gmail.com>
2026-02-05 13:44:04 +0500

    add logout function

--- src/app/components/admin/layout/Sidbar.jsx
+++ src/app/components/admin/layout/Sidbar.jsx
@@ -12,6 +12,7 @@ import {
 } from "lucide-react";
 import { cn } from "../../ui/utils";
 import useIsMobile from "../../../hook/useIsMobile";
+import { handleLogout } from "../../../../lib/utils";
 
 const navItems = [
   { label: "Dashboard", icon: LayoutDashboard, key: "dashboard" },
@@ -89,7 +90,7 @@ export default function Sidebar({ active, onChange, onLogout }) {
       <div className="px-2 py-4 border-t">
         <button
           className="flex items-center gap-3 px-3 py-2 w-full hover:bg-red-50 text-red-600 rounded-md"
-          onClick={onLogout}
+          onClick={handleLogout}
         >
           <LogOut className="w-5 h-5" />
           {!collapsed && <span>Logout</span>}
